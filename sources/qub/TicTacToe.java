98ea5716b81fdcbc7f9508306e3c07b70a91cf57
Dan Schulte <dan.schulte.qub@live.com>
2018-09-13 21:35:54 -0700

    Draw a 1 inch square on the window

--- sources/qub/TicTacToe.java
+++ sources/qub/TicTacToe.java
@@ -16,9 +16,26 @@ public class TicTacToe
 
     static void main(Process process)
     {
+        process.setUseDisplayScaling(false);
+
         try (final Window window = process.createWindow())
         {
             window.setTitle("Qub Tic-Tac-Toe");
+
+            window.setContent(new UIText(window, "Hello World!")
+            {
+                @Override
+                public void paint(UIPainter painter)
+                {
+                    super.paint(painter);
+
+                    painter.drawLine(Distance.inches(0.25), Distance.inches(0.25), Distance.inches(1), Distance.inches(0.25));
+                    painter.drawLine(Distance.inches(1), Distance.inches(0.25), Distance.inches(1), Distance.inches(1));
+                    painter.drawLine(Distance.inches(0.25), Distance.inches(0.25), Distance.inches(1), Distance.inches(1));
+                    painter.drawLine(Distance.inches(0.25), Distance.inches(0.25), Distance.inches(0.25), Distance.inches(1));
+                    painter.drawLine(Distance.inches(0.25), Distance.inches(1), Distance.inches(1), Distance.inches(1));
+                }
+            });
 
             window.setVisible(true);
             window.awaitClose();
